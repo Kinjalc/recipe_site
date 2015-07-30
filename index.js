8c3af9fccb575fc6fedad6cb44d9ac7ea86aff95
Kinjalc <kinjal.chauhan@gmail.com>
2015-07-30 17:48:32 -0400

    changes in port

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.get('/recipes/nutrition/:nutritionPick', function(req, res) {
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
-  var port = normalizePort(process.env.PORT || '3000')
+  var port = process.env.PORT || '3000';
 
 
   console.log('Example app listening at http://%s:%s', host, port);
